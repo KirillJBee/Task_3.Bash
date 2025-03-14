43aeec91faaa6792f6567a13d855d7987cbe7bbe
KirillJBee <jbeework@gmail.com>
2025-03-14 11:31:02 +0300

    init task

---
+++ Vagrantfile
@@ -0,0 +1,37 @@
+Vagrant.configure("2") do |config|
+    config.ssh.insert_key = false
+    config.vm.box = "ubuntu/jammy64"
+    config.vm.hostname = "linuxwebserver"
+    config.vm.boot_timeout = 30
+    config.vm.network "public_network", ip: "192.168.1.222",  bridge: "wlp1s0"
+    #config.vm.synced_folder ".", "/vagrant", disabled: true
+    
+    config.vm.provider "virtualbox" do |vb|
+        vb.memory = 4096
+        vb.name = "Task_3.Bash"
+    end
+
+    # Создание пользователя "user" с домашней директорией и оболочкой Bash
+    config.vm.provision "shell", inline: <<-SHELL
+    sudo useradd -m -s /bin/bash user
+    echo "user:123" | sudo chpasswd
+    sudo usermod -aG sudo user
+    echo "user ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/user
+    SHELL
+    
+    # Добавление SSH-ключа для пользователя 'user'
+    config.vm.provision "shell", inline: <<-SHELL
+    sudo mkdir -p /home/user/.ssh
+    echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYr423a9ByX9kAqD5OcukDT3kz6dmrCdid9Nn/gmPpv" >> /home/user/.ssh/authorized_keys
+    sudo chmod 700 /home/user/.ssh
+    sudo chmod 600 /home/user/.ssh/authorized_keys
+    sudo chown -R user:user /home/user/.ssh
+    SHELL
+
+    # Инсталляция файлового менеджера MC
+    config.vm.provision "shell", inline: <<-SHELL
+    sudo apt update && sudo apt -y upgrade
+    sudo apt install -y mc
+    SHELL
+
+end
---
+++ script.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+PG_VERSION="17"
+JAVA_VERSION_JDK="21"
+GRADLE_VERSION="8.5"
+REPOSITORY="https://github.com/KirillJBee/Task_2.LinuxWebServer.git"
+DIR_APP="LinuxWebServer"
+NODE_VERSION="22.9.0"
+DB_USER="java_user"
+DB_PASSWORD="12345"
+DB_NAME="java_db"
+
+   echo "Проверяем наличие  PostgreSQL..."
+    if psql -V 2>/dev/null | grep -q "PostgreSQL"; then
+        echo "$(psql -V) уже установлен."
+    else
+        echo "Устанавливаем PostgreSQL ..."
+        sudo apt update
+        sudo apt install -y curl ca-certificates gnupg
+
+        # Import the repository signing key:
+        sudo install -d /usr/share/postgresql-common/pgdg
+        sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+
+        # Create the repository configuration file:
+        sudo sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+        # Update the package lists:
+        sudo apt update
+
+        # Install the latest version of PostgreSQL:
+        #If you want a specific version, use 'postgresql-16' or similar instead of 'postgresql'
+        sudo apt -y install postgresql-$PG_VERSION
+        sudo systemctl start postgresql
+        echo "PostgreSQL $PG_VERSION установлен."
+    fi
+   
+   echo "Создание базы и пользователя PostgreSQL..."
+   sudo -u postgres psql <<EOF
+   CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
+   CREATE DATABASE $DB_NAME OWNER $DB_USER;
+   GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+EOF
+   echo "Пользователь $DB_USER и база данных $DB_NAME успешно созданы."
+
+   echo "Проверяем наличие Nginx ..."
+    if nginx -v 2>&1 | grep -q "nginx version"; then
+        echo "$(nginx -v) уже установлен."
+    else
+        echo "Устанавливаем Nginx ..."
+        sudo apt update
+        sudo apt install -y nginx
+        sudo systemctl enable nginx
+        sudo systemctl start nginx
+        echo "Nginx установлен."
+    fi
+    
+    echo "Проверяем наличие  Redis..."
+      if redis-server -v 2>/dev/null | grep -q "Redis server"; then
+        echo "$(redis-server -v) уже установлен."
+      else
+        echo "Устанавливаем Redis ..."
+        sudo apt update
+        sudo apt install -y redis-server
+        sudo systemctl start redis.service
+        echo "Redis-server установлен."
+      fi
+
+    echo "Проверяем наличие  Java OpenJDK..."
+      if  java -version 2>&1 | grep -q "openjdk version"; then
+        echo "$(java -version) уже установлен."
+      else
+        echo "Устанавливаем Java OpenJDK ..."
+        #sudo apt update
+        sudo apt install -y openjdk-$JAVA_VERSION_JDK-jdk
+        #sudo systemctl start redis.service
+        echo "Java OpenJDK установлен."
+      fi
+
+    echo "Проверяем наличие  Gradle..."
+      if  gradle -v | grep -q "$GRADLE_VERSION"; then
+        echo "$(gradle -v) уже установлен."
+      else
+        if [ -e "gradle-$GRADLE_VERSION-all.zip" ]; then
+           sudo mkdir /opt/gradle
+           sudo unzip -d /opt/gradle gradle-$GRADLE_VERSION-all.zip
+           echo "export PATH=$PATH:/opt/gradle/gradle-$GRADLE_VERSION/bin" >> ~/.bashrc
+           source ~/.bashrc
+           echo "Устанавливаем Gradle $GRADLE_VERSION ..."
+        
+      else
+        #sudo apt update
+        wget https://services.gradle.org/distributions/gradle-$GRADLE_VERSION-all.zip
+        sudo mkdir /opt/gradle
+        sudo unzip -d /opt/gradle gradle-$GRADLE_VERSION-all.zip
+        echo "export PATH=$PATH:/opt/gradle/gradle-$GRADLE_VERSION/bin" >> ~/.bashrc
+        source ~/.bashrc
+        #sudo systemctl start redis.service
+        echo "Gradle $GRADLE_VERSION установлен."
+        fi
+      fi
+
+    echo "Проверяем наличие  NodeJS..."
+      if  node -v | grep -q "v22.9.0"; then
+        echo "NodeJS $(node -v) уже установлен."
+      else
+        echo "Устанавливаем NodeJS $NODE_VERSION ..."
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+        source ~/.bashrc
+        nvm install $NODE_VERSION
+        echo "Gradle $GRADLE_VERSION установлен."
+      fi
+
+    echo "Выгрузка репозитория"
+      git clone $REPOSITORY $DIR_APP 2>/dev/null
+
+
+
+    echo "Сборка и старт front-end side"
+    cd $DIR_APP/front-end && npm install && npm run build
+    sudo mkdir -p /var/www/linuxwebserver
+    sudo cp -r build/* /var/www/linuxwebserver
+    sudo cp ~/$DIR_APP/configNginx80 /etc/nginx/sites-available/default 
+    sudo nginx -s reload
+
+# echo "Сборка и старт front-back"
+#    cd - 
+#    cd $DIR_APP/back-end && gradle bootJar
+#    env $(cat .env | xargs) java -jar build/libs/ci-back-end-0.0.1-SNAPSHOT.jar
+
